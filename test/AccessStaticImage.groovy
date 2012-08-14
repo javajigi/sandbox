53278bcc3e907aa511d77215fa705dab6fd87ec7
javajigi <javajigi@gmail.com>
2012-08-14 20:33:57 +0900

    modified image process file

--- test/AccessStaticImage.groovy
+++ test/AccessStaticImage.groovy
@@ -3,7 +3,7 @@
 	def file = new File("120810_PCroom_28291.jpg")
 	def out = new BufferedOutputStream(new FileOutputStream(file))
 	// out << new URL("http://static.archeage.com/images/game/img_ability_01.gif").openStream()
-	//out << new URL("http://postfiles9.naver.net/20120322_264/hahawonsell_1332392875629FKOPW_JPEG/k-11.jpg?type=w2").openStream()
+	out << new URL("http://postfiles9.naver.net/20120322_264/hahawonsell_1332392875629FKOPW_JPEG/k-11.jpg?type=w2").openStream()
 	out << new URL("http://postfiles4.naver.net/20120813_115/odoomi_1344809007825qGykY_JPEG/main.jpg?type=w1").openStream()
 	out.close()
 	file.delete()
