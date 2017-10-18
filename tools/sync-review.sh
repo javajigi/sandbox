6cfe759f5c8cefe79164baccbbaada171b854612
javajigi <javajigi@gmail.com>
2017-10-18 18:33:31 +0900

    refactor(sync): symc pull commits

--- tools/sync-review.sh
+++ tools/sync-review.sh
@@ -5,32 +5,33 @@ echo "current dir : $current_dir"
 
 workspace=~/codesquad/review-workspace
 
-reg="https://github.com/(.*)/(.*)/commit/(.*)"
+reg="https://github.com/code-squad/(.*)/pull/(.*)/commits/(.*)"
 
 function clone_repository {
   cd $workspace
 
-  directory=$2-$1
+  directory=$1-$2
   if [ ! -d "$directory" ]; then
     echo "clone $directory repository"
-    git clone https://github.com/$1/$2.git $directory
+    git clone https://github.com/code-squad/$1.git $directory
   fi
 }
 
-function checkout_branch {
-  cd $workspace/$2-$1
-  echo "current dir : $(pwd)"
+# function checkout_branch {
+#   cd $workspace/$1-$2
+#   echo "current dir : $(pwd)"
 
-  branch_name=$(git branch -l | grep $1)
-  echo "branch name : $branch_name"
-  if [ ! "$branch_name" ]; then
-    git checkout -t origin/$1
-  fi
-}
+#   branch_name=$(git branch -l | grep $1)
+#   echo "branch name : $branch_name"
+#   if [ ! "$branch_name" ]; then
+#     git checkout -t origin/$1
+#   fi
+# }
 
 function fetch_reset {
-  git fetch origin $1
-  git reset --hard FETCH_HEAD
+  git fetch origin pull/$1/head:pull_$1
+  git checkout pull_$1
+  git branch -a
 }
 
 if [ ! "$1" ]; then
@@ -42,8 +43,8 @@ if [[ $1 =~ $reg ]]
 then
   echo "id ${BASH_REMATCH[1]}, name ${BASH_REMATCH[2]}, commit id : ${BASH_REMATCH[3]}"
   clone_repository ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}
-  checkout_branch ${BASH_REMATCH[1]} ${BASH_REMATCH[2]}
-  fetch_reset ${BASH_REMATCH[3]}
+  cd $workspace/${BASH_REMATCH[1]}-${BASH_REMATCH[2]}
+  fetch_reset ${BASH_REMATCH[2]}
 else
   echo "does'not match"
 fi
